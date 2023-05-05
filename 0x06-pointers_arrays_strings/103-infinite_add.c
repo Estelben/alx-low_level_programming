77a653b0a10c80db9158b37216944c5dd77d01b5
Estelben <estellaomere17@gmail.com>
2023-05-04 23:52:51 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,7 +9,7 @@
 void rev_string(char *n)
 {
 int i = 0;
-int j = 0;
+int z = 0;
 char temp;
 
 while (*(n + i) != '\0')
@@ -17,10 +17,10 @@ while (*(n + i) != '\0')
 i++;
 }
 i--;
-for (j = 0; j < i; j++, i--)
+for (z = 0; z < i; z++, i--)
 {
-temp = *(n + j);
-*(n + j) = *(n + i);
+temp = *(n + z);
+*(n + z) = *(n + i);
 *(n + i) = temp;
 }
 }
@@ -37,27 +37,27 @@ temp = *(n + j);
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 
-int overflow = 0, i = 0, j = 0, digits = 0;
+int overflow = 0, i = 0, z = 0, digits = 0;
 int val1 = 0, val2 = 0, temp_tot = 0;
 
 while (*(n1 + i) != '\0')
 i++;
-while (*(n2 + j) != '\0')
-j++;
+while (*(n2 + z) != '\0')
+z++;
 i--;
-j--;
-if (j >= size_r || i >= size_r)
+z--;
+if (z >= size_r || i >= size_r)
 return (0);
-while (j >= 0 || i >= 0 || overflow == 1)
+while (z >= 0 || i >= 0 || overflow == 1)
 {
 if (i < 0)
 val1 = 0;
 else
-val1 = *(n1 + i) - '0';
-if (j < 0)
+val1 = *(n1 + i) -'0';
+if (z < 0)
 val2 = 0;
 else
-val2 = *(n2 + j) - '0';
+val2 = *(n2 + z) -'0';
 temp_tot = val1 + val2 + overflow;
 if (temp_tot >= 10)
 overflow = 1;
@@ -67,7 +67,7 @@ if (digits >= (size_r - 1))
 return (0);
 *(r + digits) = (temp_tot % 10) + '0';
 digits++;
-j--;
+z--;
 i--;
 }
 if (digits == size_r)
